8e87f818240c9f0559b8fbc90c2af35f67879a0e
Max Wang <mwang@onrisk.com>
2014-07-06 14:59:52 -0700

    Create admin dashboard--needs testing

--- config/jade.js
+++ config/jade.js
@@ -7,7 +7,7 @@ module.exports = function(app, cur_dir) {
 	// requires
 	var path = require('path');
 	// "constant" definitions
-	var view_dir = 'views';
+	var view_dir = 'views/partials/';
 	// view engine setup
 	app.set('views', path.join(cur_dir, view_dir));
 	app.set('view engine', 'jade');
--- routes/index.js
+++ routes/index.js
@@ -10,7 +10,23 @@ module.exports = function(app) {
 	var pageHandlers = require('./pages.js');
 
 	// Default
-	app.get('/', defaultHandlers.home);
+	app.get('/', pageHandlers.blog);
+
+	// For audience
+	app.get('/blog', pageHandlers.blog);
+
+	app.get('/about', pageHandlers.about);
+
+	// For Christine
+	app.get('/admin/blog', adminHandlers.blog);
+
+	app.post('/admin/blog/create', adminHandlers.blogCreate);
+
+	app.get('/admin/blog/get/:id', adminHandlers.blogGet);
+
+	app.post('/admin/blog/edit/:id', adminHandlers.blogEdit);
+
+	app.post('/admin/blog/delete/:id', adminHandlers.blogEdit);
 
 	// If url not found
 	app.get('*', defaultHandlers.index);
