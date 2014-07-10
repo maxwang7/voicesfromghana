657707398d118f09889c118ff95302db8012ab3f
Max Wang <mwang@onrisk.com>
2014-07-09 22:19:36 -0700

    Attempt at using S3

--- routes/admin.js
+++ routes/admin.js
@@ -1,4 +1,5 @@
 var Post = require('../models/post.js');
+var crypto = require('crypto');
 
 exports.dashboard = function(req, res) {
 	Post.find({}, function(err, posts) {
@@ -36,6 +37,33 @@ exports.blogCreatePOST = function(req, res, next) {
 		if(err) next(err);
 		res.redirect('/admin/dashboard');
 	});
+};
+
+exports.blogCreateSIGN = function(req, res, next) {
+	var object_name = req.query.s3_object_name + Date.now();
+	var mime_type = req.query.s3_object_type;
+	var AWS_SECRET_KEY = process.env.AWS_SECRET_KEY;
+	var AWS_ACCESS_KEY = process.env.AWS_SECRET_KEY;
+	var S3_BUCKET = process.env.S3_BUCKET;
+
+	var now = new Date();
+	var expires = Math.ceil((now.getTime() + 10000)/1000); // 10 seconds from now
+    var amz_headers = "x-amz-acl:public-read";
+
+    var put_request = "PUT\n\n"+mime_type+"\n"+expires+"\n"+amz_headers+"\n/"+S3_BUCKET+"/"+object_name;
+
+    var signature = crypto.createHmac('sha1', AWS_SECRET_KEY).update(put_request).digest('base64');
+    signature = encodeURIComponent(signature.trim());
+    signature = signature.replace('%2B','+');
+
+    var url = 'https://'+S3_BUCKET+'.s3.amazonaws.com/'+object_name;
+
+    var credentials = {
+        signed_request: url+"?AWSAccessKeyId="+AWS_ACCESS_KEY+"&Expires="+expires+"&Signature="+signature,
+        url: url
+    };
+    res.write(JSON.stringify(credentials));
+    res.end();
 };
 
 exports.blogGet = function(req, res, next) {
